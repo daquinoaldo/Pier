7b5a69564e9f67d51ed60ff620fec3220d45140c
Aldo D'Aquino <daquino.aldo@gmail.com>
2017-11-11 14:05:38 +0100

    builder: bux fix in logout.

--- builder/api/logout.php
+++ builder/api/logout.php
@@ -1,5 +1,7 @@
 <?php
+session_start();
 unset($_SESSION['username']); // will logout only
+session_commit();
 //session_destroy(); // will delete ALL data associated with that user.
 
 echo json_encode(true);
