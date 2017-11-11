80bf918bf21adcb840537e7fe67864cfbca20178
Aldo D'Aquino <daquino.aldo@gmail.com>
2017-11-11 14:12:16 +0100

    builder: divided javascript between login.js and index.js

--- builder/js/login.js
+++ builder/js/login.js
@@ -1,4 +1,3 @@
-/* LOGIN.HTML */
 function sLogin() {
     var username = document.getElementById("LUsername").value;
     var password = document.getElementById("LPassword").value;
@@ -35,50 +34,4 @@ function onRegistered(json) {
     } else {
         window.location.href = "index.html";
     }
-}
-
-function aLogout() {
-    get("api/logout.php", null, function (json) {
-        if(json.code < 0) {
-            console.error("ERROR: "+json.text);
-            alert("Can't log out. Sorry for the inconvenience. Try again later.")
-        } else {
-            window.location.href = "login.html";
-        }
-    });
-    return false;
-}
-
-/* INDEX.HTML */
-function checkLogin() {
-    /// Loading
-    get("api/get-username.php", null, function (json) {
-        if (json.code < 0) {
-            console.error("ERROR: "+json.text);
-            window.location.replace("login.html");
-        } else {
-            document.getElementById("welcome_message").innerHTML =
-                document.getElementById("welcome_message").innerHTML.replace("!", " "+json.text+"!");
-            loadWebsitesList();
-        }
-    });
-}
-
-function loadWebsitesList() {
-    get("api/get-user-websites.php", null, function (json) {
-        if (json.code < 0) {    // Not logged in: really unlikely, I just checked
-            console.error("ERROR: "+json.text);
-            window.location.replace("login.html");
-        } else {
-            var websites = json.text;
-            var table = document.getElementById("websites_table");
-            for (var i = 0; i < websites.length; i++) {
-                var row = table.insertRow(table.rows.length);
-                row.insertCell(0).innerHTML = websites[i].id;
-                row.insertCell(1).innerHTML = "<a href=\"http://"+websites[i].domain+"\">"+websites[i].domain+"</a>";
-                row.insertCell(2).innerHTML = "<a href=\"#\">manage</a>";
-            }
-            /// stop loading
-        }
-    });
 }
