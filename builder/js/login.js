d3cd2af7cdf6a837ffb299431bf52cc926881c16
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-02-09 16:50:21 +0100

    Login shows alert in case of error

--- builder/js/login.js
+++ builder/js/login.js
@@ -1,4 +1,4 @@
-function sLogin() {
+function logMeIn() {
     var username = document.getElementById("LUsername").value;
     var password = document.getElementById("LPassword").value;
     post("api/login.php", "username="+username+"&password="+password, onLoggedIn);
@@ -8,30 +8,31 @@ function sLogin() {
 function onLoggedIn(json){
     if(json.code < 0) {
         console.error("ERROR: "+json.text);
-        /// Shake form
+        alert("Can't login: "+json.text);   //TODO: Shake form
     } else {
         window.location.href = "index.html";
     }
 }
 
-function sSignUp() {
+function signMeUp() {
     var username = document.getElementById("RUsername").value;
     var email = document.getElementById("REmail").value;
     var password = document.getElementById("RPassword").value;
     var password2 = document.getElementById("RPassword2").value;
     if (password !== password2) {
-        console.error("ERROR: Password mismatching.")
-        // Shake form
+        console.error("ERROR: Password mismatching.");
+        alert("Can't sign up: password mismatching.");  //TODO: Shake form
     }
     else post("api/sign-up.php", "username="+username+"&email="+email+"&password="+password, onSignedUp);
     return false;
 }
 
 function onSignedUp(json) {
-    if(json.code < 0) {
+    if (json.code < 0) {
         console.error("ERROR: "+json.text);
-        /// Shake form
+        alert("Can't sign up: "+json.text); //TODO: Shake form
     } else {
+        alert("Signed up successfully!");
         window.location.href = "index.html";
     }
 }
