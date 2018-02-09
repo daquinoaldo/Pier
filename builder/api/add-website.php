46688a3c43e78bea7fc1164d3b2321d62297082f
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-02-09 16:50:46 +0100

    error_log stamps in case of error

--- builder/api/add-website.php
+++ builder/api/add-website.php
@@ -2,7 +2,7 @@
 require_once "functions.php";
 
 $username = getUsername();
-if(empty($username)) die(newMessage(-1, "Not logged in."));
+if (empty($username)) die(newMessage(-1, "Not logged in."));
 
 // Domain
 if (empty($_POST['domain'])) die(newMessage(-2, "Missing domain."));
@@ -10,8 +10,12 @@ $domain = htmlentities($_POST['domain'], ENT_QUOTES);
 
 // Port
 $port = getPort();
-if (empty($port)) die(newMessage(-3, "All the ports are in use, cannot allocate another port.".
-    "Consider increment the port range."));
+if (empty($port)) {
+    error_log("Error in ".__FILE__." at line ".__LINE__.":".
+    "all the ports are in use, cannot allocate another port. Consider increment the port range.");
+    die(newMessage(-3, "All the ports are in use, cannot allocate another port.".
+        "Contact the administrator."));
+}
 
 // Webserver type
 if (empty($_POST['webserver'])) die(newMessage(-4, "Missing webserver type."));
@@ -26,8 +30,15 @@ if (!empty($_POST['php']) && $_POST['php'] === "true") $php = true;
 
 // Add websites informations in database
 $id = addWebsiteToDatabase($username, $domain, $port, $webserver, $php);
-if($id < 0) die(newMessage(-6, "Error when writing to database."));
+if ($id < 0) {
+    error_log("Error in ".__FILE__." at line ".__LINE__.": error when writing to database.".
+        "username = \"$username\", domain = \"$domain\", port = \"$port\", webserver = \"$webserver\", php = \"$php\".");
+    die(newMessage(-6, "Error when writing to database."));
+}
 
-if(!builderRun($id)) die(newMessage(-7,"Cannot run the configuration. Please delete it and try again."));
+if (!builderRun($id)) {
+    error_log("Error in ".__FILE__." at line ".__LINE__.": cannot run the configuration with id $id");
+    die(newMessage(-7,"Cannot run the configuration. Please delete it and try again."));
+}
 
 echo newMessage(-10, "Website created.");
