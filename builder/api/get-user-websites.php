6d266c9693635ed75b7d100f625dac9fb9202839
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-02-12 20:01:32 +0100

    Performance improved

--- builder/api/get-user-websites.php
+++ builder/api/get-user-websites.php
@@ -4,7 +4,7 @@ require_once "functions.php";
 $username = getUsername();
 if(empty($username)) die(newMessage(-1, "Not logged in."));
 
-$result = getWebsitesList($_SESSION['username']);
+$result = getWebsitesList($username);
 $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 echo newMessage(0, $rows);
