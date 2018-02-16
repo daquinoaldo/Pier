9342636d911909062eeeef07985133334c180bf3
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-02-16 16:47:01 +0100

    Added FOREIGN_KEY constraint

--- sql-initdb.d/init.sql
+++ sql-initdb.d/init.sql
@@ -24,7 +24,8 @@ CREATE TABLE `websites` (
   `port` INT NOT NULL UNIQUE,
   `webserver` VARCHAR(6) NOT NULL,
   `php` BOOLEAN NOT NULL,
-  `mysql` BOOLEAN NOT NULL
+  `mysql` BOOLEAN NOT NULL,
+  FOREIGN KEY (username) REFERENCES users(username)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 COMMIT;
