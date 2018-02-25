1f07264ca9a0e0865e83dd8df86e39b3805e898b
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-02-25 18:46:26 +0100

    Shebang and safe use of variable

--- add-user.sh
+++ add-user.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ "$#" -ne 3 ]
 then
 	echo "You must pass this 3 arguments: username, password, home"
@@ -5,11 +6,12 @@ else
 	username=$1
 	password=$2
 	home=$3
-	useradd $username
-	uid="$(id -u $username)"
-	gid="$(id -G $username)"
-	echo $uid
-	echo $gid
-	( echo $password ; echo $password ) | pure-pw useradd $username -u $uid -g $gid -d $home
+	useradd ${username}
+	uid="$(id -u ${username})"
+	gid="$(id -G ${username})"
+	echo ${uid}
+	echo ${gid}
+	( echo ${password} ; echo ${password} ) | pure-pw useradd ${username} -u ${uid} -g ${gid} -d ${home}
 	pure-pw mkdb
+	chmod -R 777 ${home}
 fi
